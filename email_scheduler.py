841769012335954b432711ea1481eb43b7f50f21
Deeb <yehya.ar.eldeeb@gmail.com>
2021-05-13 18:20:42 +0200

    improves scheduling sending email

--- email_scheduler.py
+++ email_scheduler.py
@@ -2,24 +2,34 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
 import datetime
 import os
+# import logging
 
 scheduler = None
 
 def deadline_reached(event_id, email, question):
-    # send to email the question with url for results
-    result = os.popen('python3 send_email.py -data_event_id "{}" -email "{}" -question "{}"'.format(event_id, email, question)).read()
+    os.system('python3 send_email.py --data_event_id "{}" --to_email "{}" --question "{}" &'.format(event_id, email, question))
 
 def start_scheduler(database_url):
     global scheduler
     jobstores = {
         'default': SQLAlchemyJobStore(url=database_url)
     }
-    scheduler = BackgroundScheduler(jobstores=jobstores)
+    scheduler = BackgroundScheduler(jobstores=jobstores, job_defaults={'misfire_grace_time':3600})
     scheduler.start()
+    # logging.basicConfig()
+    # log = logging.getLogger('apscheduler')
+    # log.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter(fmt='%(asctime)s|%(levelname)s%(name)s|%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    # stream_handler = logging.StreamHandler()
+    # file_handler = logging.FileHandler('apscheduler.log', encoding='utf-8')
+    # stream_handler.setFormatter(formatter)
+    # file_handler.setFormatter(formatter)
+    # log.addHandler(stream_handler)
+    # log.addHandler(file_handler)
 
 def add_job(event_id, timestamp, email, question):
     global scheduler
-    scheduler.add_job(deadline_reached, 'date', run_date=timestamp, id=event_id, replace_existing=True, coalesce=True, args=[event_id, email, question])
+    scheduler.add_job(deadline_reached, 'date', run_date=timestamp, id=event_id, replace_existing=True, args=[event_id, email, question])
 
 def shutdown_scheduler():
     global scheduler
--- send_email.py
+++ send_email.py
@@ -6,34 +6,34 @@ import datetime
 
 
 parser = argparse.ArgumentParser("Send emails via Mailjet!")
-parser.add_argument('to_email')
-parser.add_argument('to_name')
-parser.add_argument('question')
-parser.add_argument('link')
-parser.add_argument('data_event_id')
-parser.add_argument('data_subject', default='Greetings', nargs='*')
+parser.add_argument('--to_email')
+parser.add_argument('--question')
+parser.add_argument('--data_event_id')
+parser.add_argument('--data_subject', default='Greetings', nargs='*')
 # parser.add_argument('data_text_part', default='Hello World!', nargs='*')
 # parser.add_argument('data_html_part', default='<b>Hello World</b>', nargs='*')
-parser.add_argument('data_custom_id', default='customid', nargs='*')
+parser.add_argument('--data_custom_id', default='customid', nargs='*')
 args, unkwn = parser.parse_known_args()
 load_dotenv()
 api_key = os.getenv('API_KEY')
 api_secret = os.getenv('API_SECRET')
-from_email = os.getenv("FROM_EMAIL")
-from_name = os.getenv("FROM_NAME")
+from_email = os.getenv('FROM_EMAIL')
+from_name = os.getenv('FROM_NAME')
+base_url = os.getenv('BASE_URL')
 to_email = args.to_email
-to_name = args.to_name
+question = args.question
 data_subject = args.data_subject
 data_event_id = args.data_event_id
 data_custom_id = args.data_custom_id
+link = 'http://{}/event/{}/result'.format(base_url, data_event_id)
 data_html_part = '''
     <h1>Greetings from DoWhatWhen.</h1>
     Your event with question {} has now reached the voting deadline. Please visit the result at <a href='{}'>this link</a>.
-'''.format(args.question, args.link)
+'''.format(question, link)
 data_text_part = '''
     Greetings from DoWhatWhen.
     Your event with question {} has now reached the voting deadline. Please visit the result at the following link: {}
-'''.format(args.question, args.link)
+'''.format(question, link)
 data = {
   'Messages': [
     {
@@ -44,7 +44,7 @@ data = {
       "To": [
         {
           "Email": to_email,
-          "Name": to_name
+          "Name": to_email
         }
       ],
       "Subject": data_subject,
@@ -57,5 +57,4 @@ data = {
 mailjet = Client(auth=(api_key, api_secret), version='v3.1')
 result = mailjet.send.create(data=data)
 with open('email_log.txt', 'a') as f:
-    f.write('{}: {}, {}\n'.format(datetime.datetime.now(), data_event_id, result.status_code))
-exit(result.status_code)
+    f.write('{}: {}, {}, {}\n'.format(datetime.datetime.now(), data_event_id, result.status_code, result.json()))
